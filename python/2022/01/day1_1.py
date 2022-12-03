ad161db9c37bfd5c80e1a16868300f44e2102598
SWRZASD <akos.fekete87@gmail.com>
2022-12-03 21:54:37 +0100

    Updated solution

--- python/2022/01/day1_1.py
+++ python/2022/01/day1_1.py
@@ -1,10 +1,3 @@
-filename = 'list.txt'
-
-# import os
-# path = f"{os.getcwd()}\\{filename}"
-# file = open(path, 'r')
-# list = file.read().splitlines()
-# calories_list = []
 
 with open('list.txt') as f:
     cal = 0
@@ -22,43 +15,8 @@ with open('list.txt') as f:
     sum_cal.sort(reverse=True)
 
     print(sum(sum_cal[0:3]))
-    print(sum_cal)
-    print(sum_cal[0] + sum_cal[1] + sum_cal[2])
 
-    for index, value in enumerate(sum_cal):
-        if value == 62093:
-            print(index)
+    # print(sum_cal)
+    # print(sum_cal[0] + sum_cal[1] + sum_cal[2])
 
-    
-
-        # print(line)
-
-
-
-# for i in list:
-#     try:
-#         e = int(i)
-#         calories_list.append(e)
-#     except:
-#         calories_list.append(0)
-#
-# def valami():
-#     cal = 0
-#     max_cal = 0
-#     elf = 0
-#     max_elf = 0
-#
-#     for i in calories_list:
-#         cal += i
-#         if i == 0:
-#             elf += 1
-#             if cal > max_cal:
-#                 max_cal = cal
-#                 max_elf = elf
-#             cal = 0
-#
-#     print(cal)
-#     print(max_cal)
-#     print(max_elf)
-#
-# valami()
+    # print(line)
